7f46022a5826a62dcd8393647076e88e7f8cb340
Elif <elifintizam@gmail.com>
2023-06-29 15:05:05 +0300

    Generic List

--- src/Week5/Hw_ListClass/Main.java
+++ src/Week5/Hw_ListClass/Main.java
@@ -16,28 +16,21 @@ public class Main {
 
         System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
 
-        // Bulduğu ilk indeksi verir
         System.out.println("Indeks : " + liste.indexOf(20));
 
-        // Bulamazsa -1 döndürür
         System.out.println("Indeks :" + liste.indexOf(100));
 
-        // Bulduğu son indeksi verir
         System.out.println("Indeks : " + liste.lastIndexOf(20));
 
-        // Listeyi Object[] dizisi olarak geri verir.
         Object[] dizi = liste.toArray();
         System.out.println("Object dizisinin ilk elemanı :" + dizi[0]);
 
-        // Liste veri türünde alt bir liste oluşturdu
         MyList<Integer> altListem = liste.subList(0, 3);
         System.out.println(altListem.toString());
 
-        // Değerim listedeki olup olmadığını sorguladı
         System.out.println("Listemde 20 değeri : " + liste.contains(20));
         System.out.println("Listemde 120 değeri : " + liste.contains(120));
 
-        // Listeyi tamamen boşaltır ve varsayılan boyutuna çevirir
         liste.clear();
         System.out.println(liste);
 

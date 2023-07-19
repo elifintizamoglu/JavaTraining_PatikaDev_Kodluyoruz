25edc75ef216291f21d18baeecec6c43262a9ae0
Elif <elifintizam@gmail.com>
2023-07-19 19:06:30 +0300

    Thread Race

--- src/Week6/Hw_ThreadRace/Hw_ThreadRace.java
+++ src/Week6/Hw_ThreadRace/Hw_ThreadRace.java
@@ -6,9 +6,8 @@ import java.util.List;
 public class Hw_ThreadRace implements Runnable {
 
     private List<Integer> list;
-    private static List<Integer> evenNumbers=new ArrayList<>();
-    private static List<Integer> oddNumbers= new ArrayList<>();
-    private final Object LOCK = new Object();
+    private static List<Integer> evenNumbers = new ArrayList<>();
+    private static List<Integer> oddNumbers = new ArrayList<>();
 
     public Hw_ThreadRace(List<Integer> list) {
         this.list = list;
@@ -16,39 +15,20 @@ public class Hw_ThreadRace implements Runnable {
 
     @Override
     public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        for (int number : this.list) {
+        for (Integer number : this.list) {
             if (number % 2 == 0) {
-                synchronized (LOCK){
-                    this.evenNumbers.add(number);
-                }
+                evenNumbers.add(number);
             } else {
-                synchronized (LOCK){
-                    this.oddNumbers.add(number);
-                }
-
+                oddNumbers.add(number);
             }
         }
-        printEvenNumber();
-        printOddNumber();
     }
 
-    public void printEvenNumber() {
-        System.out.println("Even Numbers: ");
-        for (int number : evenNumbers) {
-            System.out.println(Thread.currentThread().getName() + " : " + number + " ");
-        }
+    public static List<Integer> printEvenNumbers() {
+        return evenNumbers;
     }
 
-    public void printOddNumber() {
-        System.out.println("Odd numbers: ");
-        for (int number : oddNumbers) {
-            System.out.println(Thread.currentThread().getName() + " : " + number + " ");
-        }
+    public static List<Integer> printOddNumbers() {
+        return oddNumbers;
     }
 }
--- src/Week6/Hw_ThreadRace/Main.java
+++ src/Week6/Hw_ThreadRace/Main.java
@@ -2,47 +2,38 @@ package Week6.Hw_ThreadRace;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 10000; i++) {
             list.add(i);
         }
 
-        /*List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i <= 100; i += 25) {
-            Thread t = new Thread(new Hw_ThreadRace(list.subList(i, i += 25)));
-            threads.add(t);
-            t.start();
-        }*/
+        ExecutorService pool = Executors.newFixedThreadPool(4);
 
-        List<Integer> subList1 = list.subList(0, 25);
-        List<Integer> subList2 = list.subList(25, 50);
-        List<Integer> subList3 = list.subList(50, 75);
-        List<Integer> subList4 = list.subList(75, 100);
-
-        Thread t1 = new Thread(new Hw_ThreadRace(subList1));
-        Thread t2 = new Thread(new Hw_ThreadRace(subList2));
-        Thread t3 = new Thread(new Hw_ThreadRace(subList3));
-        Thread t4 = new Thread(new Hw_ThreadRace(subList4));
-
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-
-
-        /*System.out.println("Odd numbers: ");
-        for (int odd : Hw_ThreadRace.oddNumbers) {
-            System.out.print(odd + " ");
+        int x, y;
+        for (int i = 0; i < 4; i++) {
+            x = i * 2500;
+            y = x + 2500;
+            pool.execute(new Hw_ThreadRace(list.subList(x, y)));
+            Thread.sleep(1000);
         }
 
-        System.out.println("Even numbers: ");
-        for (int even : Hw_ThreadRace.evenNumbers) {
-            System.out.print(even + " ");
+        pool.shutdown();
+        try {
+            pool.awaitTermination(5, TimeUnit.SECONDS);
+            System.out.println("Threads are completed.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-*/
+
+        System.out.println("Odd Numbers: " + Hw_ThreadRace.printOddNumbers());
+        System.out.println("Even Numbers: " + Hw_ThreadRace.printEvenNumbers());
+
     }
 }
